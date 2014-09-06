24fbcbecab7243005cd74f95c3e27b7a606c8bb3
yuchango <yuchango@gmail.com>
2014-09-06 14:55:01 +0900

    Accelerometer with pitch and roll info

---
+++ arduino/accelerometer/measureAcce/measureAcce.ino
@@ -0,0 +1,63 @@
+#include <ADXL335.h>
+
+/*****************************************************************************/
+//	Function:  Get the accelemeter of the x/y/z axis. 
+//  Hardware:  Grove - 3-Axis Analog Accelerometer
+//	Arduino IDE: Arduino-1.0
+//	Author:	 Frankie.Chu		
+//	Date: 	 Jan 11,2013
+//	Version: v1.0
+//	by www.seeedstudio.com
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+/*******************************************************************************/
+
+//#include <ADXL335.h>
+
+ADXL335 accelerometer;
+void setup()
+{
+	Serial.begin(9600);
+	accelerometer.begin();
+}
+void loop()
+{
+//	int x,y,z;
+//	accelerometer.getXYZ(&x,&y,&z);
+//	Serial.print("value of X/Y/Z: ");
+//	Serial.print(x);
+//	Serial.print(" x, ");
+//	Serial.print(y);
+//	Serial.print(" y, ");
+//	Serial.print(z);
+//	Serial.print(" z, ");
+	float ax,ay,az, pitch, roll;
+	accelerometer.getAcceleration(&ax,&ay,&az);
+	accelerometer.getPitchAndRoll(&roll,&pitch);
+	// Serial.println("accleration of X/Y/Z: ");
+	Serial.print(ax*9.81);
+	Serial.print(" \t, ");
+	Serial.print(ay*9.81);
+	Serial.print(" \t, ");
+	Serial.print(az*9.81);
+	Serial.print(" \t, pitch");
+       	Serial.print(pitch);
+	Serial.print("\t, roll");
+        Serial.println(roll);
+	//delay(500);
+
+
+}
